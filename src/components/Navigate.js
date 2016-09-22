12803b52cb96016c145b3c038ef5981d29516218
Eric Ferraiuolo <edf@ericf.me>
2016-09-21 23:53:18 -0400

    Add <Navigate> component for react-router@4

---
+++ src/components/Navigate.js
@@ -0,0 +1,23 @@
+import {Component, PropTypes} from 'react';
+import {router as routerShape} from 'react-router/PropTypes';
+
+export default class Navigate extends Component {
+  static contextTypes = {
+    router: routerShape.isRequired,
+  };
+
+  static propTypes = {
+    to: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.object
+    ]).isRequired,
+  };
+
+  componentDidMount() {
+    this.context.router.transitionTo(this.props.to);
+  }
+
+  render() {
+    return null;
+  }
+}
