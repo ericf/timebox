5330e2fb8f0c8c36c8fe3a1877ab961f9967b820
Eric Ferraiuolo <eferraiuolo@gmail.com>
2016-10-27 03:16:00 -0400

    Add script to check environment variables

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ const app = firebase.initializeApp({
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DATABASE_URL,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
 });
 
 const TimeboxApp = () => (
